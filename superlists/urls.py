d8b436448b8fb3741cc92213744b9a6354ee5ced
leishunbo <leishunbo@gmail.com>
2017-12-17 23:56:32 +0800

    Initial commit

---
+++ superlists/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'superlists.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', admin.site.urls),
+]
