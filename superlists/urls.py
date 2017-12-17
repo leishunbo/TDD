731d147bbbcc94999e07e4c2142d987031cfeccf
leishunbo <leishunbo@gmail.com>
2017-12-18 01:08:29 +0800

    chapter_03

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from lists import views
 admin.autodiscover()
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
+    url(r'^$', views.home_page, name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', admin.site.urls),
